da9d4ed7b232869f04fd814a63edaa8e2aa906c6
Priyanshu Rastogi <priyanshu8642@gmail.com>
2023-03-08 02:52:43 -0700

    made some UI changes

--- app/src/main/java/com/example/qrhunter/fragments/SearchPlayerFragment.java
+++ app/src/main/java/com/example/qrhunter/fragments/SearchPlayerFragment.java
@@ -3,26 +3,29 @@ package com.example.qrhunter.fragments;
 import static android.content.ContentValues.TAG;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.qrhunter.CustomAdapter;
 import com.example.qrhunter.R;
+import com.example.qrhunter.UserListItem;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.auth.User;
 
 import java.util.ArrayList;
 
@@ -31,13 +34,12 @@ public class SearchPlayerFragment extends Fragment {
     final FirebaseFirestore db = FirebaseFirestore.getInstance();
     final CollectionReference collectionReference = db.collection("Users");
     ListView playerListView;
-    ArrayList<String> usernames;
-    ArrayAdapter<String> usernamesArrayAdapter;
+    ArrayList<UserListItem> usernames;
+    CustomAdapter usernamesArrayAdapter;
 
     public SearchPlayerFragment() {
         // Required empty public constructor
     }
-
 
 
     @Override
@@ -49,7 +51,7 @@ public class SearchPlayerFragment extends Fragment {
 
         playerListView = view.findViewById(R.id.player_list_list_view);
 
-        usernames = new ArrayList<String>();
+        usernames = new ArrayList<UserListItem>();
 
         collectionReference.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
@@ -57,20 +59,18 @@ public class SearchPlayerFragment extends Fragment {
                 if (task.isSuccessful()) {
                     for (QueryDocumentSnapshot document : task.getResult()) {
                         String username = document.getString("UserName");
-                        usernames.add(username);
+                        usernames.add(new UserListItem(username, 0));
                     }
                 } else {
                     Log.d(TAG, "Error getting documents: ", task.getException());
                 }
 
-                usernamesArrayAdapter = new ArrayAdapter<String>(getContext(), R.layout.player_list_items_text_view, usernames);
+                usernamesArrayAdapter = new CustomAdapter(getContext(), usernames);
                 playerListView.setAdapter(usernamesArrayAdapter);
             }
         });
 
         return view;
-
-
     }
 
 }
