ebb6516575774c68fbbc96c3aa8bc7d6f70fdec8
“Zihan <zihanhossain99@gmail.com>
2023-03-12 23:02:17 -0600

    Added javadocs for PlaceholderMapFragment

--- app/src/main/java/com/example/qrhunter/fragments/PlaceholderMapFragment.java
+++ app/src/main/java/com/example/qrhunter/fragments/PlaceholderMapFragment.java
@@ -11,9 +11,24 @@ import androidx.fragment.app.Fragment;
 
 import com.example.qrhunter.R;
 
+/**
+ * Fragment responsible for displaying an image as the actual Map functionality has not been implemented
+ */
 public class PlaceholderMapFragment extends Fragment {
     private View view;
 
+    /**
+     * Called to create the view hierarchy associated with the fragment. This method is responsible for
+     * inflating the fragment's layout and returning the root View of the inflated layout. If the fragment
+     * does not have a UI or does not need to display a view, you can return null from this method.
+     *
+     * @param inflater           The LayoutInflater object that can be used to inflate any views in the fragment.
+     * @param container          The parent view that the fragment's UI should be attached to. This value may be null
+     *                           if the fragment is not being attached to a parent view.
+     * @param savedInstanceState A Bundle containing any saved state information for the fragment. This value may be null
+     *                           if the fragment is being instantiated for the first time.
+     * @return The View for the fragment's UI, or null.
+     */
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
---
+++ app/src/test/java/com/example/qrhunter/WalletUnitTest.java
@@ -0,0 +1,12 @@
+package com.example.qrhunter;
+
+import java.util.ArrayList;
+
+public class WalletUnitTest {
+
+    private ArrayList<QRCode> qrList() {
+        ArrayList<QRCode> list = new ArrayList<>();
+        cityList.add(mockCity());
+        return cityList;
+    }
+}
