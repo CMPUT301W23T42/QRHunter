b28af0f0a3714e8199dc25179e93a8db2e70e6ae
Daniel Castro <dcastro@ualberta.ca>
2023-03-12 19:49:44 -0600

    Fixed java docs for profile related fragments

--- app/src/main/java/com/example/qrhunter/TabManager.java
+++ app/src/main/java/com/example/qrhunter/TabManager.java
@@ -22,7 +22,7 @@ public class TabManager implements UserInfo {
     private FragmentManager fragmentManager;
     private Fragment currentFragment;
 
-    /*
+    /**
      * TabManager initializer
      * @param fragmentActivity  object representing main activity
      */
@@ -30,7 +30,7 @@ public class TabManager implements UserInfo {
         fragmentManager = fragmentActivity.getSupportFragmentManager();
     }
 
-    /*
+    /**
      * Utilizes fragment transaction to replace fragment on display
      * @param   fragment    fragment object to place on display
      */
@@ -41,7 +41,7 @@ public class TabManager implements UserInfo {
         transaction.commit();
     }
 
-    /*
+    /**
      * Switches main fragment display based on parameter
      * @param   position    integer representation of fragment to display
      */
@@ -107,7 +107,7 @@ public class TabManager implements UserInfo {
         Log.d(TAG, "Fragment Type: " + currentFragment.getClass().toString());
     }
 
-    /*
+    /**
      * Set the profile attribute of object
      * @param   profile UserProfile object representing the user profile
      */
@@ -120,7 +120,7 @@ public class TabManager implements UserInfo {
         }
     }
 
-    /*
+    /**
      * Set profile on profile fragment attribute on changes to object
      */
     @Override
--- app/src/main/java/com/example/qrhunter/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/qrhunter/fragments/ProfileFragment.java
@@ -18,6 +18,9 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+/**
+ * Fragment responsible for display profile to users
+ */
 public class ProfileFragment extends Fragment implements UserInfo {
     private onEditProfileListener listener;
     private final String TAG = "Profile Fragment";
@@ -29,7 +32,7 @@ public class ProfileFragment extends Fragment implements UserInfo {
     private TextView emailText;
     private TextView phoneText;
 
-    /*
+    /**
      * Profile fragment initializer
      * @param profile UserProfile object representing a user profile
      */
@@ -37,6 +40,18 @@ public class ProfileFragment extends Fragment implements UserInfo {
         this.profile = profile;
     }
 
+    /**
+     * Called to create the view hierarchy associated with the fragment. This method is responsible for
+     * inflating the fragment's layout and returning the root View of the inflated layout. If the fragment
+     * does not have a UI or does not need to display a view, you can return null from this method.
+     *
+     * @param inflater           The LayoutInflater object that can be used to inflate any views in the fragment.
+     * @param container          The parent view that the fragment's UI should be attached to. This value may be null
+     *                           if the fragment is not being attached to a parent view.
+     * @param savedInstanceState A Bundle containing any saved state information for the fragment. This value may be null
+     *                           if the fragment is being instantiated for the first time.
+     * @return The View for the fragment's UI, or null.
+     */
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
@@ -55,7 +70,7 @@ public class ProfileFragment extends Fragment implements UserInfo {
         return view;
     }
 
-    /*
+    /**
      * Set the profile attribute of object
      * @param   profile UserProfile object representing the user profile
      */
@@ -66,7 +81,7 @@ public class ProfileFragment extends Fragment implements UserInfo {
         onChange();
     }
 
-    /*
+    /**
      * Updates view elements on any changes to object
      */
     @Override
@@ -81,7 +96,7 @@ public class ProfileFragment extends Fragment implements UserInfo {
         }
     }
 
-    /*
+    /**
      * Sets listener attribute of profile fragment object
      * @param   listener    represents an onEditProfileListener
      */
