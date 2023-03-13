bdc3105425fd0846897347cff27a21884fe19ab5
“Zihan <zihanhossain99@gmail.com>
2023-03-12 23:15:45 -0600

    j

--- app/src/test/java/com/example/qrhunter/WalletUnitTest.java
+++ app/src/test/java/com/example/qrhunter/WalletUnitTest.java
@@ -1,12 +1,20 @@
 package com.example.qrhunter;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 
 public class WalletUnitTest {
 
     private ArrayList<QRCode> qrList() {
         ArrayList<QRCode> list = new ArrayList<>();
-        cityList.add(mockCity());
-        return cityList;
+        list.add(new QRCode("test date", "test hash", "test name", null, "test owner", 7, "test id"));
+        return list;
+    }
+
+    @Test
+    void testCountPoints() {
+        ArrayList<QRCode> list = qrList();
+
     }
 }
