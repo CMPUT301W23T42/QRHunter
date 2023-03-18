e396b5c40e71ab7dff700fd12787e8be0856c3c6
Priyanshu Rastogi <priyanshu8642@gmail.com>
2023-03-18 02:01:00 -0600

    added interactive map

--- app/src/main/java/com/example/qrhunter/fragments/MapFragment.java
+++ app/src/main/java/com/example/qrhunter/fragments/MapFragment.java
@@ -10,13 +10,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.qrhunter.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 /**
  * Fragment responsible for displaying an image as the actual Map functionality has not been implemented
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
     private View view;
-
+    private GoogleMap mMap;
     /**
      * Called to create the view hierarchy associated with the fragment. This method is responsible for
      * inflating the fragment's layout and returning the root View of the inflated layout. If the fragment
@@ -35,6 +38,19 @@ public class MapFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.qr_map);
+        mapFragment.getMapAsync(this);
+
+
         return view;
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        // Customize the map here
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
 }
