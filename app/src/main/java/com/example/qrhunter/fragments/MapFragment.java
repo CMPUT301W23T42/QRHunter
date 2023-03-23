2b4666c351055a109c34c67eec5f0653d23cadad
Priyanshu Rastogi <priyanshu8642@gmail.com>
2023-03-23 00:29:53 -0600

    random

--- app/src/main/java/com/example/qrhunter/fragments/MapFragment.java
+++ app/src/main/java/com/example/qrhunter/fragments/MapFragment.java
@@ -1,37 +1,46 @@
 package com.example.qrhunter.fragments;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.qrhunter.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.OnMapsSdkInitializedCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
-/**
- * Fragment responsible for displaying an image as the actual Map functionality has not been implemented
- */
-public class MapFragment extends Fragment implements OnMapReadyCallback {
+import java.util.List;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback, LocationListener, OnMapsSdkInitializedCallback {
     private View view;
     private GoogleMap mMap;
-    /**
-     * Called to create the view hierarchy associated with the fragment. This method is responsible for
-     * inflating the fragment's layout and returning the root View of the inflated layout. If the fragment
-     * does not have a UI or does not need to display a view, you can return null from this method.
-     *
-     * @param inflater           The LayoutInflater object that can be used to inflate any views in the fragment.
-     * @param container          The parent view that the fragment's UI should be attached to. This value may be null
-     *                           if the fragment is not being attached to a parent view.
-     * @param savedInstanceState A Bundle containing any saved state information for the fragment. This value may be null
-     *                           if the fragment is being instantiated for the first time.
-     * @return The View for the fragment's UI, or null.
-     */
+    private LocationManager locationManager;
+    private static final long MIN_TIME = 400;
+    private static final float MIN_DISTANCE = 1000;
+    private final int MY_PERMISSIONS_REQUEST_LOCATION = 1;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -43,8 +52,45 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 .findFragmentById(R.id.qr_map);
         mapFragment.getMapAsync(this);
 
+        //check for location access and ask for permission if not
+        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getContext(), "First enable LOCATION ACCESS", Toast.LENGTH_LONG).show();
+            ActivityCompat.requestPermissions(getActivity(), new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+            return null;
+        }
+
+        locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
+
+        MapsInitializer.initialize(getActivity().getApplicationContext(), MapsInitializer.Renderer.LATEST, this);
 
         return view;
+    }
+
+    @Override
+    public void onMapsSdkInitialized(MapsInitializer.Renderer renderer) {
+        switch (renderer) {
+            case LATEST:
+                Log.d("MapsDemo", "The latest version of the renderer is used.");
+                break;
+            case LEGACY:
+                Log.d("MapsDemo", "The legacy version of the renderer is used.");
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_LOCATION) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, access the location
+            } else {
+                // Permission denied
+            }
+        }
     }
 
     @Override
@@ -52,5 +98,12 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         mMap = googleMap;
         // Customize the map here
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 5);
+        mMap.animateCamera(cameraUpdate);
     }
 }
